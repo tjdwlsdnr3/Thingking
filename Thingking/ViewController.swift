0a2a7ab96b2f4796817eb8389ae9a8442ef4270f
Jinuk Sung <jinuksung@jinukui-macbook-pro.local>
2018-04-23 18:18:07 +0900

    Initial Commit

---
+++ Thingking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thingking
+//
+//  Created by Jinuk Sung on 2018. 4. 23..
+//  Copyright © 2018년 Studio Nuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
