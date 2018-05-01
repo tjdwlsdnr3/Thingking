61be6ae8b3d8018088b27f2a3826cf84e5d1fe94
Jinuk Sung <jinuksung@jinukui-macbook-pro.local>
2018-05-01 15:04:50 +0900

    SWRevealViewController 기능 넣음. 사이드뷰 생성 확인함

---
+++ Thingking/ViewController/Controller/FrontViewController.swift
@@ -0,0 +1,37 @@
+//
+//  FrontViewController.swift
+//  Thingking
+//
+//  Created by Jinuk Sung on 2018. 5. 1..
+//  Copyright © 2018년 Studio Nuki. All rights reserved.
+//
+
+import UIKit
+
+class FrontViewController: UIViewController {
+
+    @IBOutlet var btnSideMenu: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Thingking/ViewController/Controller/RearViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RearViewCell.swift
+//  Thingking
+//
+//  Created by Jinuk Sung on 2018. 5. 1..
+//  Copyright © 2018년 Studio Nuki. All rights reserved.
+//
+
+import UIKit
+
+class RearViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
